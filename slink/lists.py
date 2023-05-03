adedf1eee8984a14304aacac4c0da9108ab1f04f
jgfranco17 <chino.franco@gmail.com>
2023-05-03 19:19:57 +0900

    Added doubly-linked list

--- slink/lists.py
+++ slink/lists.py
@@ -143,3 +143,118 @@ class LinkedList:
             current = next_node
         # Return prev in order to put the head at the end
         self.head = prev
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        """
+        For iterators to access and iterate through data inside doubly-linked list.
+        """
+        node = self.head
+        while node:
+            yield node.data
+            node = node.next
+
+    def __str__(self):
+        return "->".join([str(item) for item in self])
+
+    def __len__(self):
+        return sum(1 for _ in self)
+
+    def insert_at_head(self, data):
+        self.insert_at_nth(0, data)
+
+    def insert_at_tail(self, data):
+        self.insert_at_nth(len(self), data)
+
+    def insert_at_nth(self, index: int, data):
+        """
+        Insert data at specified index.
+        """
+        length = len(self)
+
+        if not 0 <= index <= length:
+            raise IndexError("List index out of range")
+
+        new_node = Node(data)
+        if self.head is None:
+            self.head = self.tail = new_node
+        elif index == 0:
+            self.head.previous = new_node
+            new_node.next = self.head
+            self.head = new_node
+        elif index == length:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
+        else:
+            temp = self.head
+            for _ in range(0, index):
+                temp = temp.next
+            temp.previous.next = new_node
+            new_node.previous = temp.previous
+            new_node.next = temp
+            temp.previous = new_node
+
+    def delete_head(self):
+        return self.delete_at_nth(0)
+
+    def delete_tail(self):
+        return self.delete_at_nth(len(self) - 1)
+
+    def delete_at_nth(self, index: int):
+        """
+        Delete data at specified index.
+        """
+        length = len(self)
+
+        if not 0 <= index <= length - 1:
+            raise IndexError("list index out of range")
+        delete_node = self.head  # default first node
+        if length == 1:
+            self.head = self.tail = None
+        elif index == 0:
+            self.head = self.head.next
+            self.head.previous = None
+        elif index == length - 1:
+            delete_node = self.tail
+            self.tail = self.tail.previous
+            self.tail.next = None
+        else:
+            temp = self.head
+            for _ in range(0, index):
+                temp = temp.next
+            delete_node = temp
+            temp.next.previous = temp.previous
+            temp.previous.next = temp.next
+        return delete_node.data
+
+    def delete(self, data) -> str:
+        current = self.head
+
+        while current.data != data:  # Find the position to delete
+            if current.next:
+                current = current.next
+            else:  # We have reached the end an no value matches
+                raise ValueError("No data matching given value")
+
+        if current == self.head:
+            self.delete_head()
+
+        elif current == self.tail:
+            self.delete_tail()
+
+        else:  # Before: 1 <--> 2(current) <--> 3
+            current.previous.next = current.next  # 1 --> 3
+            current.next.previous = current.previous  # 1 <--> 3
+        return data
+
+    def is_empty(self):
+        """
+        Check if list is empty.
+        """
+        return len(self) == 0
