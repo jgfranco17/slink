12649575bda2a637fdee8db881ef8a5092a63b74
jgfranco17 <chino.franco@gmail.com>
2023-05-06 20:18:20 +0900

    Updated tests for other functions of Stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,20 +1,72 @@
 import pytest
+from slink.stacks import Stack
 from slink.structexceptions import StackOverflowError, StackUnderflowError
-from .conftest import sample_stack
+from .conftest import base_stack
+
+
+def test_stack_init(base_stack):
+    assert isinstance(base_stack, Stack)
+    assert base_stack.limit == 10
+
+
+def test_stack_init_with_limit():
+    base_stack = Stack(limit=5)
+    assert base_stack.limit == 5
+
+
+def test_stack_push(base_stack):
+    base_stack.push(1)
+    assert base_stack.size == 1
+    assert base_stack.peek() == 1
+    base_stack.push(2)
+    assert base_stack.size == 2
+    assert base_stack.peek() == 2
 
 
 def test_stack_underflow(sample_stack):
-    """
-    Test that popping an empty stack raises StackUnderflowError.
-    """
     with pytest.raises(StackUnderflowError):
         _ = sample_stack.pop()
 
 
 def test_stack_overflow(sample_stack):
-    """
-    Test that exceeding the stack limit raises StackOverflowError.
-    """
     with pytest.raises(StackOverflowError):
         for value in range(sample_stack.limit + 1):
             sample_stack.push(value)
+
+
+def test_stack_pop(base_stack):
+    base_stack.push(1)
+    base_stack.push(2)
+    assert base_stack.pop() == 2
+    assert base_stack.size == 1
+    assert base_stack.pop() == 1
+    assert base_stack.size == 0
+
+
+def test_stack_pop_empty(base_stack):
+    with pytest.raises(StackUnderflowError):
+        base_stack.pop()
+
+
+def test_stack_peek(base_stack):
+    base_stack.push(1)
+    assert base_stack.peek() == 1
+    base_stack.push(2)
+    assert base_stack.peek() == 2
+
+
+def test_stack_peek_empty(base_stack):
+    with pytest.raises(StackUnderflowError):
+        base_stack.peek()
+
+
+def test_stack_len(base_stack):
+    assert len(base_stack) == 0
+    base_stack.push(1)
+    assert len(base_stack) == 1
+
+
+def test_stack_str(base_stack):
+    base_stack.push(1)
+    base_stack.push(2)
+    assert str(base_stack) == '[1 2]'
