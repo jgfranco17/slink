c415c7e624640ba0ce441236474994e5dc6a1ecc
jgfranco17 <chino.franco@gmail.com>
2023-05-06 20:17:02 +0900

    Wrote fixtures for linked lists

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,37 @@
 import pytest
 from slink.stacks import Stack
+from slink.nodes import ListNode
+from slink.lists import LinkedList, DoublyLinkedList
 
 
-# each test runs on cwd to its temp dir
-@pytest.fixture(scope="module")
-def sample_stack():
-    stack = Stack(limit=3)
-    return stack
+@pytest.fixture
+def base_stack():
+    return Stack()
+
+
+@pytest.fixture
+def empty_list():
+    return LinkedList()
+
+
+@pytest.fixture
+def non_empty_list():
+    llist = LinkedList()
+    llist.add(1)
+    llist.add(2)
+    llist.add(3)
+    return llist
+
+
+@pytest.fixture
+def doubly_empty_list():
+    return DoublyLinkedList()
+
+
+@pytest.fixture
+def doubly_non_empty_list():
+    dlist = DoublyLinkedList()
+    dlist.insert_at_head(1)
+    dlist.insert_at_head(2)
+    dlist.insert_at_head(3)
+    return dlist
