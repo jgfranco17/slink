2f55ab6503c013084abf0d3eb8997080ba37bd62
jgfranco17 <chino.franco@gmail.com>
2023-05-06 19:54:48 +0900

    Wrote fixture for sample stack

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,9 @@
-import sys
 import pytest
+from slink.stacks import Stack
 
 
 # each test runs on cwd to its temp dir
-@pytest.fixture(autouse=True)
-def go_to_tmpdir(request):
-    # Get the fixture dynamically by its name.
-    tmpdir = request.getfixturevalue("tmpdir")
-    # ensure local test created packages can be imported
-    sys.path.insert(0, str(tmpdir))
-    # Chdir only for the duration of the test.
-    with tmpdir.as_cwd():
-        yield
+@pytest.fixture(scope="module")
+def sample_stack():
+    stack = Stack(limit=3)
+    return stack
