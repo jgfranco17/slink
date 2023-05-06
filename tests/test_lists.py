089dea8162a55c0f2b38e6d143fb31e2d47448c4
jgfranco17 <chino.franco@gmail.com>
2023-05-06 20:17:42 +0900

    Added tests for methods + attributes

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -1,5 +1,85 @@
-from slink.lists import LinkedList
+import pytest
+from slink.nodes import ListNode
+from slink.lists import LinkedList, DoublyLinkedList
 
 
-def test_base():
-    assert True
+def test_linked_list_init():
+    llist = LinkedList()
+    assert llist.head is None
+
+
+def test_linked_list_len(empty_list, non_empty_list):
+    assert len(empty_list) == 0
+    assert len(non_empty_list) == 3
+
+
+def test_linked_list_repr(empty_list, non_empty_list):
+    assert str(empty_list) == ""
+    assert str(non_empty_list) == "1 -> 2 -> 3"
+
+
+def test_linked_list_add(empty_list):
+    empty_list.add(1)
+    assert len(empty_list) == 1
+    assert empty_list.head.data == 1
+
+
+def test_linked_list_insert(empty_list):
+    empty_list.insert(1)
+    assert len(empty_list) == 1
+    assert empty_list.head.data == 1
+
+
+def test_linked_list_insert_nth(empty_list):
+    empty_list.insert_nth(0, 1)
+    assert len(empty_list) == 1
+    assert empty_list.head.data == 1
+    empty_list.insert_nth(1, 2)
+    assert len(empty_list) == 2
+    assert empty_list.head.next.data == 2
+
+
+def test_linked_list_is_empty(empty_list, non_empty_list):
+    assert empty_list.is_empty is True
+    assert non_empty_list.is_empty is False
+
+
+def test_doubly_linked_list_init():
+    dlist = DoublyLinkedList()
+    assert dlist.head is None
+    assert dlist.tail is None
+
+
+def test_doubly_linked_list_len(doubly_empty_list, doubly_non_empty_list):
+    assert len(doubly_empty_list) == 0
+    assert len(doubly_non_empty_list) == 3
+
+
+def test_doubly_linked_list_insert_at_head(doubly_empty_list):
+    doubly_empty_list.insert_at_head(1)
+    assert len(doubly_empty_list) == 1
+    assert doubly_empty_list.head.data == 1
+    assert doubly_empty_list.tail.data == 1
+
+
+def test_doubly_linked_list_insert_at_tail(doubly_empty_list):
+    doubly_empty_list.insert_at_tail(1)
+    assert len(doubly_empty_list) == 1
+    assert doubly_empty_list.head.data == 1
+    assert doubly_empty_list.tail.data == 1
+
+
+def test_doubly_linked_list_insert_at_nth(doubly_empty_list):
+    doubly_empty_list.insert_at_nth(0, 1)
+    assert len(doubly_empty_list) == 1
+    assert doubly_empty_list.head.data == 1
+    assert doubly_empty_list.tail.data == 1
+    doubly_empty_list.insert_at_nth(1, 2)
+    assert len(doubly_empty_list) == 2
+    assert doubly_empty_list.head.next.data == 2
+    assert doubly_empty_list.tail.data == 2
+
+
+def test_doubly_linked_list_is_empty(doubly_empty_list, doubly_non_empty_list):
+    assert doubly_empty_list.is_empty() is True
+    assert doubly_non_empty_list.is_empty() is False
