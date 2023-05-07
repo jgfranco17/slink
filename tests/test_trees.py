4cbe830b36836f442ee6fd090b07fbba50458d01
jgfranco17 <chino.franco@gmail.com>
2023-05-07 12:50:27 +0900

    Added unittests for tree structure

---
+++ tests/test_trees.py
@@ -0,0 +1,20 @@
+import pytest
+from slink.nodes import TreeNode
+from slink.trees import BinaryTree
+from .conftest import empty_tree_node, simple_tree_node, basic_tree
+
+
+def test_tree_node_init(simple_tree_node):
+    assert simple_tree_node.data == 5
+    assert simple_tree_node.left is None
+    assert simple_tree_node.right is None
+
+
+def test_tree_node_bool(empty_tree_node, simple_tree_node):
+    assert bool(simple_tree_node) == True
+    assert bool(empty_tree_node) == False
+
+
+def test_tree_init(simple_tree_node):
+    tree = BinaryTree(simple_tree_node)
+    assert tree.root == simple_tree_node
