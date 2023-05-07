786301000c70cbb43c98a0ace207405e75c2f8c6
jgfranco17 <chino.franco@gmail.com>
2023-05-07 12:07:49 +0900

    Refactored tree-crawling algorithm

--- slink/trees.py
+++ slink/trees.py
@@ -1,17 +1,23 @@
 from typing import Any
+from .nodes import TreeNode
 
 
 class BinaryTree:
     """
     A BinaryTree has data variable and pointers to Nodes to its left and right.
     """
-    def __init__(self, data: Any) -> None:
-        self.data = data
-        self.left: BinaryTree | None = None
-        self.right: BinaryTree | None = None
-        
+    def __init__(self, root: TreeNode) -> None:
+        self.root = root
+
+    def __bool__(self):
+        return self.data is not None
+
+    @property
     def depth(self):
+        return self.__get_depth(self.root)
+        
+    def __get_depth(self, node):
         """
         Recursive function that returns the depth of a binary tree.
         """
-        return 1 + max(self.depth(self.left), self.depth(self.right)) if self else 0
+        return 1 + max(self.depth(node.left), self.depth(node.right)) if node else 0
