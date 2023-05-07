d446f062d924fe34be320ec294a1754e93daffec
jgfranco17 <chino.franco@gmail.com>
2023-05-07 12:06:49 +0900

    Formatting edits

--- slink/nodes.py
+++ slink/nodes.py
@@ -3,18 +3,13 @@ from typing import Any
 
 class ListNode:
     def __init__(self, data: Any):
-        """
-        Create and initialize ListNode class instance.
-        """
         self.data = data
         self.next = None
 
     def __repr__(self) -> str:
-        """
-        Get the string representation of this node.
-        """
-        return f"ListNode({self.data})"
+        return f'<ListNode at {hex(id(self))}, data={self.data}>'
 
     @property
     def address(self):
         return hex(id(self))
+
