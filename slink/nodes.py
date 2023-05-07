6cdab542da6372bf3d843ead3cb166c3c233a5b1
jgfranco17 <chino.franco@gmail.com>
2023-05-07 14:07:49 +0900

    Created class for graph node

--- slink/nodes.py
+++ slink/nodes.py
@@ -22,3 +22,18 @@ class TreeNode:
 
     def __bool__(self):
         return self.data is not None
+
+
+class GraphNode:
+    def __init__(self, data: Any) -> None:
+        self.data = data
+        self.neighbors = []
+
+    def __repr__(self) -> str:
+        return f'<GraphNode at {hex(id(self))}, data={self.data}>'
+
+    def add_neighbor(self, neighbor):
+        """
+        Adds a neighboring node to the node's list of neighbors.
+        """
+        self.neighbors.append(neighbor)
