6ba6a8930a581d417e0929271efe6ddc2e1433db
jgfranco17 <chino.franco@gmail.com>
2023-05-07 19:53:12 +0900

    Updated separator for str representation

--- slink/stacks.py
+++ slink/stacks.py
@@ -23,7 +23,7 @@ class Stack:
         return f'<Stack at {hex(id(self))}, limit={self.__limit}>'
 
     def __str__(self) -> str:
-        items = " ".join(list(map(str, self.__stack)))
+        items = "|".join(list(map(str, self.__stack)))
         return f'[{items}]'
 
     @property
