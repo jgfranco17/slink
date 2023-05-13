8a32e7c0fc053294addc5a167c9c7eba814713e5
jgfranco17 <chino.franco@gmail.com>
2023-05-13 20:41:25 +0900

    Updated BinaryTree methods

--- slink/trees.py
+++ slink/trees.py
@@ -1,23 +1,49 @@
 from typing import Any
-from .nodes import TreeNode
-
+from .nodes import BinaryTreeNode
 
 class BinaryTree:
     """
-    A BinaryTree has data variable and pointers to Nodes to its left and right.
+    A Tree node has data variable and pointers to nodes to its left and right.
     """
-    def __init__(self, root: TreeNode) -> None:
+    def __init__(self, root: BinaryTreeNode) -> None:
         self.root = root
-
-    def __bool__(self):
-        return self.data is not None
+        self.left: BinaryTree | None = None
+        self.right: BinaryTree | None = None
 
     @property
     def depth(self):
-        return self.__get_depth(self.root)
-        
-    def __get_depth(self, node):
+        return self.__depth_of_tree(self.root)
+
+    @property
+    def is_full_binary_tree(self):
+        return self.__check_full_tree(self.root)
+
+    def __display(self, tree) -> None:  # In Order traversal of the tree
+        """
+        Display the tree.
+        """
+        if tree:
+            self.__display(tree.left)
+            print(tree.data)
+            self.__display(tree.right)
+
+    def display(self) -> None:
+        self.__display(self.root)
+
+    def __depth_of_tree(self, tree) -> int:
         """
         Recursive function that returns the depth of a binary tree.
         """
-        return 1 + max(self.depth(node.left), self.depth(node.right)) if node else 0
+        return 1 + max(self.__depth_of_tree(tree.left), self.__depth_of_tree(tree.right)) if tree else 0
+
+    def __check_full_tree(self, tree) -> bool:
+        """
+        Returns True if this is a full binary tree.
+        """
+        if not tree:
+            return True
+        if tree.left and tree.right:
+            return self.is_full_binary_tree(tree.left) and self.is_full_binary_tree(tree.right)
+        else:
+            return not tree.left and not tree.right
+    
